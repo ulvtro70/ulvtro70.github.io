cf056064b348b097c8d2e4681a7a3bae0aac2a70
ulvtro70 <115401186+ulvtro70@users.noreply.github.com>
2022-10-14 14:40:08 +0200

    Oppdatering

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "dea901c916243a64ce27e89b267ce783",
-"/": "dea901c916243a64ce27e89b267ce783",
-"main.dart.js": "73f0325ec29d5d6590ba15ea2344932f",
+"index.html": "6546ba1042854eb7e0c7ae5db75d5852",
+"/": "6546ba1042854eb7e0c7ae5db75d5852",
+"main.dart.js": "9b56e6e560b24bce2135d3dc238075f6",
 "manifest.json": "dd7696aa408a20f5e37789eda6aa48df",
 "version.json": "48458f1b04632a5a108c2882dc383253"
 };
